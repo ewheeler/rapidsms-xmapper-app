9d84065813b82dfb1f849d91b4679108cfc1efa9
ewheeler <evanmwheeler@gmail.com>
2010-06-23 12:37:28 -0400

    some small things for making heatmaps using http://sethoscope.net/heatmap/

--- urls.py
+++ urls.py
@@ -3,5 +3,6 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = patterns('',
-    url(r"^xmapper/$", views.index, name="xmapper")
+    url(r"^xmapper/$", views.index, name="xmapper"),
+    url(r"^xmapper/xlocxport$", views.export_for_heatmap)
 )
--- views.py
+++ views.py
@@ -1,9 +1,13 @@
+import csv
+import random
+
+from django.http import HttpResponse
 from django.views.decorators.http import require_GET, require_POST
 from django.shortcuts import redirect, get_object_or_404
 from django.conf import settings
 
 from rapidsms.utils import render_to_response
-from .models import XLoc
+from .models import XLoc, Place
 
 @require_GET
 def index(req): 
@@ -26,3 +30,24 @@ def index(req):
 
     breadcrumbs = (('Map', ''),)
     return render_to_response(req, "xmapper/dashboard.html", { 'xlocs_by_key': xlocs_by_key, 'breadcrumbs': breadcrumbs } )
+
+def export_for_heatmap(req):
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=team_1_disp_under5.csv'
+    #response['Content-Disposition'] = 'attachment; filename=team_1_disp_6_to_14.csv'
+
+    writer = csv.writer(response)
+
+    xlocs = XLoc.objects.filter(group__name="all")
+    #xlocs = Place.objects.all()
+
+    for xloc in xlocs:
+        sub_dict = xloc.submitted_data
+        data = int(sub_dict["disp_under5"])
+        #data = int(sub_dict["disp_6_to_14"])
+
+        for times in range(data):
+            writer.writerow([xloc.lat, xloc.lng])
+        #for times in range(random.randrange(0, 100), random.randrange(100,5000)):
+        #    writer.writerow([xloc.point.latitude, xloc.point.longitude])
+    return response
